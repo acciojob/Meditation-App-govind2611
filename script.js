ebeeb4e39756529be6eeefe42675eea6028203d3
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-11-12 12:05:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,67 @@
-//your JS code here. If required.
+const song = document.querySelector(".song");
+const video = document.querySelector(".vid-container video");
+const play = document.querySelector(".play");
+const outline = document.querySelector(".moving-outline circle");
+const outlineLength = outline.getTotalLength();
+let fakeDuration = 600;
+function togglePlay(song) {
+    if (song.paused) {
+        song.play();
+        video.play()
+        play.src = './svg/pause.svg';
+
+    } else {
+        song.pause();
+        video.pause();
+        play.src = './svg/play.svg';
+    }
+}
+
+const app = () => {
+  const timeDisplay = document.querySelector(".time-display");
+  const timeSelect = document.querySelectorAll('.time-select button');
+
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
+
+  play.addEventListener("click", () => {
+      togglePlay(song);
+  });
+
+  timeSelect.forEach(option => {
+    option.addEventListener("click", function() {
+      fakeDuration = this.getAttribute("data-time");
+      timeDisplay.textContent = Math.floor(fakeDuration / 60) + ":" + Math.floor(fakeDuration % 60);
+    });
+  });
+
+  const sounds = document.querySelectorAll(".sound-picker button");
+  sounds.forEach(sound => {
+    sound.addEventListener("click", function() {
+      song.src = this.getAttribute("data-sound");
+      video.src = this.getAttribute("data-video");
+      togglePlay(song);
+    });
+  });
+
+  song.ontimeupdate = () => {
+    let currentTime = song.currentTime;
+    let elapsed = fakeDuration - currentTime;
+    let seconds = Math.floor(elapsed % 60);
+    let minutes = Math.floor(elapsed / 60);
+
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    outline.style.strokeDashoffset = progress;
+
+    timeDisplay.textContent = minutes + ":" + seconds;
+
+    if (currentTime >= fakeDuration) {
+      song.pause();
+      song.currentTime = 0;
+      play.src = "./svg/play.svg";
+      video.pause();
+    }
+  };
+};
+
+app();
